cd4641d5587860d0a97fd2cb0d5494afca60cbc0
juliardnz <89615651+juliardnz@users.noreply.github.com>
2021-10-03 17:17:25 -0300

    Add files via upload
    Hands on Work III - Sistema Lounge Bar Finalizado

---
+++ LoungeBarOK/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace LoungeBarOK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgCadastro = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNomeBebida = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAtivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbBebida = new System.Windows.Forms.Label();
+            this.txtBebida = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.lbCategoria = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lbDescricao = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lbValor = new System.Windows.Forms.Label();
+            this.btAdicionar = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCadastro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgCadastro
+            // 
+            this.dgCadastro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCadastro.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colNomeBebida,
+            this.colCategoria,
+            this.colDescricao,
+            this.colValor,
+            this.colAtivo});
+            this.dgCadastro.Location = new System.Drawing.Point(12, 199);
+            this.dgCadastro.Name = "dgCadastro";
+            this.dgCadastro.Size = new System.Drawing.Size(675, 285);
+            this.dgCadastro.TabIndex = 0;
+            this.dgCadastro.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgCadastro_CellContentClick);
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            this.colID.Width = 30;
+            // 
+            // colNomeBebida
+            // 
+            this.colNomeBebida.HeaderText = "Bebida";
+            this.colNomeBebida.Name = "colNomeBebida";
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descricao";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.Width = 300;
+            // 
+            // colValor
+            // 
+            this.colValor.HeaderText = "Valor";
+            this.colValor.Name = "colValor";
+            this.colValor.Width = 50;
+            // 
+            // colAtivo
+            // 
+            this.colAtivo.HeaderText = "Status";
+            this.colAtivo.Name = "colAtivo";
+            this.colAtivo.Width = 50;
+            // 
+            // lbBebida
+            // 
+            this.lbBebida.AutoSize = true;
+            this.lbBebida.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBebida.Location = new System.Drawing.Point(9, 9);
+            this.lbBebida.Name = "lbBebida";
+            this.lbBebida.Size = new System.Drawing.Size(51, 13);
+            this.lbBebida.TabIndex = 1;
+            this.lbBebida.Text = "Bebida:";
+            // 
+            // txtBebida
+            // 
+            this.txtBebida.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBebida.Location = new System.Drawing.Point(100, 9);
+            this.txtBebida.Name = "txtBebida";
+            this.txtBebida.Size = new System.Drawing.Size(424, 21);
+            this.txtBebida.TabIndex = 2;
+            this.txtBebida.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.Location = new System.Drawing.Point(100, 49);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(424, 21);
+            this.txtCategoria.TabIndex = 4;
+            this.txtCategoria.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbCategoria
+            // 
+            this.lbCategoria.AutoSize = true;
+            this.lbCategoria.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCategoria.Location = new System.Drawing.Point(9, 49);
+            this.lbCategoria.Name = "lbCategoria";
+            this.lbCategoria.Size = new System.Drawing.Size(68, 13);
+            this.lbCategoria.TabIndex = 3;
+            this.lbCategoria.Text = "Categoria:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.Location = new System.Drawing.Point(100, 88);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(424, 60);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // lbDescricao
+            // 
+            this.lbDescricao.AutoSize = true;
+            this.lbDescricao.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescricao.Location = new System.Drawing.Point(9, 105);
+            this.lbDescricao.Name = "lbDescricao";
+            this.lbDescricao.Size = new System.Drawing.Size(68, 13);
+            this.lbDescricao.TabIndex = 5;
+            this.lbDescricao.Text = "Descricao:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(100, 164);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(280, 21);
+            this.txtValor.TabIndex = 8;
+            this.txtValor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbValor
+            // 
+            this.lbValor.AutoSize = true;
+            this.lbValor.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbValor.Location = new System.Drawing.Point(9, 164);
+            this.lbValor.Name = "lbValor";
+            this.lbValor.Size = new System.Drawing.Size(41, 13);
+            this.lbValor.TabIndex = 7;
+            this.lbValor.Text = "Valor:";
+            // 
+            // btAdicionar
+            // 
+            this.btAdicionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btAdicionar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAdicionar.Location = new System.Drawing.Point(553, 9);
+            this.btAdicionar.Name = "btAdicionar";
+            this.btAdicionar.Size = new System.Drawing.Size(134, 31);
+            this.btAdicionar.TabIndex = 9;
+            this.btAdicionar.Text = "Adicionar";
+            this.btAdicionar.UseVisualStyleBackColor = false;
+            this.btAdicionar.Click += new System.EventHandler(this.btAdicionar_Click);
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btAlterar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAlterar.Location = new System.Drawing.Point(553, 59);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(134, 29);
+            this.btAlterar.TabIndex = 10;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = false;
+            this.btAlterar.Click += new System.EventHandler(this.btAlterar_Click);
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btLimpar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLimpar.Location = new System.Drawing.Point(551, 105);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(136, 31);
+            this.btLimpar.TabIndex = 11;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = false;
+            this.btLimpar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btExcluir.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExcluir.Location = new System.Drawing.Point(553, 154);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(134, 31);
+            this.btExcluir.TabIndex = 12;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = false;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(464, 164);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(60, 21);
+            this.txtId.TabIndex = 14;
+            this.txtId.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(396, 164);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(30, 13);
+            this.lblId.TabIndex = 13;
+            this.lblId.Text = "ID: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 496);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.btAdicionar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lbValor);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.lbDescricao);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.lbCategoria);
+            this.Controls.Add(this.txtBebida);
+            this.Controls.Add(this.lbBebida);
+            this.Controls.Add(this.dgCadastro);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Bebida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCadastro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgCadastro;
+        private System.Windows.Forms.Label lbBebida;
+        private System.Windows.Forms.TextBox txtBebida;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label lbCategoria;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lbDescricao;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lbValor;
+        private System.Windows.Forms.Button btAdicionar;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNomeBebida;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAtivo;
+    }
+}
+
